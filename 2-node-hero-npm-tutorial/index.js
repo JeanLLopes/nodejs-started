54bde39d4cc9190faf31d768b4f6e281890e4649
JeanLLopes <jean.llopes@outlook.com>
2017-03-21 23:38:47 -0300

    Adding Development Dependencies

--- 2-node-hero-npm-tutorial/index.js
+++ 2-node-hero-npm-tutorial/index.js
@@ -1,3 +1,9 @@
 //for start this file use
 //npm start or node index.js
 require('./app/index')
+
+
+//CLASS 2
+const _ = require('lodash')
+_.assign({ 'a': 1 }, { 'b': 2 }, { 'c': 3 });  
+// → { 'a': 1, 'b': 2, 'c': 3 }
